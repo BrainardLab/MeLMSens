1cd95d8fad830d4cdfc49ba02a066320803e3f15
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 00:15:43 -0400

    Extract plotting function mel low/high SPDs

--- R/schematic.plot.R
+++ R/schematic.plot.R
@@ -24,3 +24,28 @@ plot_receptors <- function (receptors) {
     )
   return(p)
 }
+
+plot_lowhigh <- function(SPDs) {
+  p <- ggplot(data = SPDs) +
+    aes(x = Wls) +
+    geom_path(aes(y = Mel_high), size = 1, alpha = .66, color = "red") +
+    geom_path(aes(y = Mel_low), size = 1, alpha = .66, color = "black") +
+    #  geom_path(aes(y = LMS_low, color = "a"), size =2, alpha = 0) +
+    
+    # Labels
+    ylab("Radiometric Power") +
+    xlab("Wavelength (nm)") +
+    scale_y_continuous(breaks = FALSE) +
+    scale_color_discrete(guide = "none") +
+    
+    # theme
+    theme_bw() +  
+    theme(text = element_text(size=14),
+          strip.background =element_rect(fill="#01256e"),
+          strip.text = element_text(colour = 'white'),
+          axis.title.y = element_text(margin = margin(r = .25, unit = "in")),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank()
+    )
+  return(p)
+}
