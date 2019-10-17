4f5cc79e037467a5bc201b10578a79eed09b9d52
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 15:36:16 -0400

    Export to CSV

---
+++ matlab/+MeLMSens_SteadyAdapt/exportFlickerSplatterToCSV.m
@@ -0,0 +1,10 @@
+function exportFlickerSplatterToCSV()
+%EXPORTRESULTSTOCSV Exports all flicker splatter to CSV-file
+%   Detailed explanation goes here
+
+% Get results
+results = MeLMSens_SteadyAdapt.dataManagement.functionForAll(@MeLMSens_SteadyAdapt.splatter.flickerContrastsFromName);
+
+% Write to file
+writetable(results,fullfile(getpref('MeLMSens','dataDir'),'experimental','adapt.flicker.splatter.csv'));
+end
