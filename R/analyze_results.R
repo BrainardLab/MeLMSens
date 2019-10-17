afa9dc9887e5af9f7ddf4cc1ff887ecb0baa22f2
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 01:33:41 -0400

    Bugfix results plot

--- R/analyze_results.R
+++ R/analyze_results.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 median_JNDs <- function (JNDs) {
   median_JNDs <- JNDs %>%
-    group_by(participant, axis, level) %>%
+    group_by(participant, level) %>%
     summarise(median = median(JND),
               SEM = 1.253*sd(JND, na.rm=TRUE)/sqrt(length(JND)),
               MminSEM = median-SEM,
--- R/results.plot.R
+++ R/results.plot.R
@@ -6,9 +6,9 @@ plot_JNDs <- function (JNDs, medianJNDs) {
     geom_line(data = JNDs, aes(x = level, y = JND, group=session), alpha = .7) +
     
     # Participant median and SEM
-    geom_point(data = median_JNDs, aes(x = level, y = median), size = 1) +
-    geom_line(data = median_JNDs, aes(x = level, y = median, group = 1), size =1) +
-    geom_linerange(data = median_JNDs, aes(x = level, ymin = MminSEM, ymax = MplusSEM), size = 1) +
+    geom_point(data = medianJNDs, aes(x = level, y = median), size = 1) +
+    geom_line(data = medianJNDs, aes(x = level, y = median, group = 1), size =1) +
+    geom_linerange(data = medianJNDs, aes(x = level, ymin = MminSEM, ymax = MplusSEM), size = 1) +
     
     # Markup
     facet_grid(~ participant) +
