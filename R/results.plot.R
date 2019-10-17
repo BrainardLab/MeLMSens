21b3b40241567c1aaf1ac194eeaaa28d58e12e55
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 01:03:26 -0400

    Plot only melanopsin results

--- R/results.plot.R
+++ R/results.plot.R
@@ -10,8 +10,8 @@ plot_JNDs <- function (JNDs, medianJNDs) {
     geom_linerange(data = median_JNDs, aes(x = level, ymin = MminSEM, ymax = MplusSEM), size = 1) +
     
     # Markup
-    facet_grid(participant ~ axis) +
-    scale_y_continuous(breaks = seq(-1,7,1)) +
+    facet_grid(~ participant) +
+    scale_y_continuous(breaks = seq(-1,7,1), limits = c(-1,5)) +
     ylab("Normalized Flicker Threshold") +
     xlab("Adaptation level") +
     theme_bw()
