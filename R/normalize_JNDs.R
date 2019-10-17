a0f01f24838e3e6cac2d41d7dc203d646720ea23
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 03:16:24 -0400

    Fix normalize JNDs

--- R/normalize_JNDs.R
+++ R/normalize_JNDs.R
@@ -3,7 +3,7 @@ require(tidyverse)
 normalize_JNDs <- function(data){
   data <- data %>%
     # Select only relevant columns
-    select(-threshold_contrast, -LMS_background) %>%
+    select(participant, session, axis, level, threshold_JND) %>%
     
     # Convert to wide-format: separate columns for high/low
     spread(key = level, value = threshold_JND) %>%
