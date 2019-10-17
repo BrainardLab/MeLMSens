ed63475bee4ec152a085c4901b42a73988416186
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 01:40:50 -0400

    Add pulse data loading function

--- R/load_data.R
+++ R/load_data.R
@@ -1,6 +1,14 @@
+require(tidyverse)
+
 load_adapt <- function() {
-  require(tidyverse)
   JNDs <- read_csv(here('data','experimental','adapt.results.csv'), na = c("", "NA", "NaN")) %>%
+    mutate(level = factor(level)) %>%
+    mutate(axis = factor(axis))
+  return(JNDs)
+}
+
+load_pulse <- function() {
+  JNDs <- read_csv(here('data','experimental','pulse.results.csv'), na = c("", "NA", "NaN")) %>%
     mutate(level = factor(level)) %>%
     mutate(axis = factor(axis))
   return(JNDs)
