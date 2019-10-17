9555629727705d9d94312fc0166ddbd28f1255d8
Joris Vincent <joris.vincent@gmail.com>
2019-10-17 00:54:07 -0400

    Extract median JNDs function

---
+++ R/analyze_results.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+median_JNDs <- function (JNDs) {
+  median_JNDs <- JNDs %>%
+    group_by(participant, axis, level) %>%
+    summarise(median = median(JND),
+              SEM = 1.253*sd(JND, na.rm=TRUE)/sqrt(length(JND)),
+              MminSEM = median-SEM,
+              MplusSEM = median+SEM)
+  return(median_JNDs)
+}
